cb985893c9a5daaa5cd55e189c135107f993b9b3
duc <1025434218@qq.com>
2023-02-17 09:08:23 +0800

    Update maotai.go

--- cronjob/maotai/maotai.go
+++ cronjob/maotai/maotai.go
@@ -10,5 +10,5 @@ func init() {
 	cronjob.Manager().NewCommand("maotai", func(robot bot.Bot) error {
 		robot.SendGroup(config.GroupId, "开始申购茅台了~")
 		return nil
-	}).DailyAt("9:00")
+	}).DailyAt("9:15")
 }
