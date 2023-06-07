8e88f29000a10a808b8a6dc16466806e5a5a77c6
duc <1025434218@qq.com>
2023-06-07 17:05:54 +0800

    wip

--- features/pixiv/pixiv.go
+++ features/pixiv/pixiv.go
@@ -154,6 +154,7 @@ func Image(content string) (string, error) {
 	base := filepath.Base(a.Image.Original)
 	fpath := filepath.Join("/data", "images", base)
 
+	os.MkdirAll(filepath.Join("/data", "image"), 0755)
 	err = func() error {
 		file, err := os.OpenFile(fpath, os.O_TRUNC|os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
