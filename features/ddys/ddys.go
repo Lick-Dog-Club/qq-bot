24cd43e30b20201bcb392f66d9cb7f9381e026ee
duc <1025434218@qq.com>
2023-07-03 12:09:12 +0800

    ddys

---
+++ features/ddys/ddys.go
@@ -0,0 +1,192 @@
+package ddys
+
+import (
+	"bytes"
+	"net/http"
+	"qq/bot"
+	"qq/features"
+	"qq/features/util/proxy"
+	"strings"
+	"sync"
+	"text/template"
+	"time"
+
+	"github.com/antchfx/htmlquery"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	features.AddKeyword("ddys", "<+dy/dm>, 获取更新的电影/动漫, 默认 +dy", func(bot bot.Bot, content string) error {
+		for _, m := range Get(content, 3*24*time.Hour) {
+			bot.Send(m.String())
+		}
+		return nil
+	})
+}
+
+type movie struct {
+	Url          string
+	Title        string
+	Director     string
+	Year         string
+	Kind         string
+	HeadImageUrl string
+	Rating       string
+	Country      string
+	UpdateAt     time.Time
+}
+
+var temp, _ = template.New("").Parse(`
+{{ .Title }} -- ({{ .Director }} {{ .Year }})
+类型：{{ .Kind }}
+评分：{{ .Rating }}
+国家：{{ .Country }}
+影片地址：{{ .Url }}
+
+[CQ:image,file={{.HeadImageUrl}}]
+`)
+
+func (m *movie) String() string {
+	bf := bytes.Buffer{}
+	temp.Execute(&bf, m)
+	return bf.String()
+}
+
+func (m *movie) isNew(duration time.Duration) bool {
+	now := time.Now()
+	fromDate := time.Date(now.Year(), now.Month(), now.Day(), 9, 10, 0, 0, time.Local).Add(-duration)
+
+	return m.UpdateAt.After(fromDate)
+}
+
+func buildRequest(url string) *http.Request {
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+	return request
+}
+
+func Get(param string, duration time.Duration) (res []movie) {
+	url := "https://ddys.art/category/movie/"
+	if param == "dm" {
+		url = "https://ddys.art/category/anime/new-bangumi/"
+	}
+	do, _ := proxy.NewHttpProxyClient().Do(buildRequest(url))
+	defer do.Body.Close()
+	doc, err := htmlquery.Parse(do.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 获取电影详情页的 url
+	var (
+		articleDetailUrlCh chan string = make(chan string, 20)
+		wg                             = sync.WaitGroup{}
+		resultCh           chan movie  = make(chan movie, 20)
+	)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case path, ok := <-articleDetailUrlCh:
+					if !ok {
+						log.Println("articleDetailUrlCh done")
+						return
+					}
+					resultCh <- fetchDetail(path)
+				}
+			}
+		}()
+	}
+
+	find := htmlquery.Find(doc, "//article")
+	for _, node := range find {
+		for _, attribute := range node.Attr {
+			if attribute.Key == "data-href" {
+				articleDetailUrlCh <- attribute.Val
+			}
+		}
+	}
+	close(articleDetailUrlCh)
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+	for ch := range resultCh {
+		if ch.isNew(duration) {
+			res = append(res, ch)
+		}
+	}
+
+	return res
+}
+
+func fetchDetail(url string) (m movie) {
+	log.Println(url)
+	do, _ := proxy.NewHttpProxyClient().Do(buildRequest(url))
+	defer do.Body.Close()
+	parse, err := htmlquery.Parse(do.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m.Url = url
+
+	// Title
+	title := htmlquery.Find(parse, `//div[@class="post-content"]/h1/text()`)
+	for _, node := range title {
+		m.Title = node.Data
+	}
+	// updated
+	updated := htmlquery.Find(parse, `//li[@class="meta_date"]/time`)
+	for _, node := range updated {
+		for _, attribute := range node.Attr {
+			if attribute.Key == "datetime" {
+				t, _ := time.Parse(time.RFC3339, attribute.Val)
+				m.UpdateAt = t
+			}
+		}
+	}
+	// abstract
+	abstract := htmlquery.Find(parse, `//div[@class="abstract"]/text()`)
+	for _, node := range abstract {
+		if strings.HasPrefix(node.Data, "导演") {
+			m.Director = content(node.Data)
+		}
+		if strings.HasPrefix(node.Data, "类型") {
+			m.Kind = content(node.Data)
+		}
+		if strings.HasPrefix(node.Data, "制片国家") {
+			m.Country = content(node.Data)
+		}
+		if strings.HasPrefix(node.Data, "年份") {
+			m.Year = content(node.Data)
+		}
+	}
+
+	// image
+	image := htmlquery.Find(parse, `//div[@class="post"]/img`)
+	for _, node := range image {
+		for _, attribute := range node.Attr {
+			if attribute.Key == "src" {
+				m.HeadImageUrl = attribute.Val
+			}
+		}
+	}
+	// rating
+	rating := htmlquery.Find(parse, `//div[@class="rating"]/span[@class="rating_nums"]/text()`)
+	for _, node := range rating {
+		m.Rating = node.Data
+	}
+
+	return
+}
+
+func content(data string) string {
+	split := strings.Split(data, " ")
+	if len(split) >= 2 {
+		return split[1]
+	}
+	return ""
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/NateScarlet/pixiv v0.7.0
 	github.com/adshao/go-binance/v2 v2.4.2
+	github.com/antchfx/htmlquery v1.3.0
 	github.com/cenkalti/backoff/v4 v4.2.0
 	github.com/duc-cnzj/when-rules v0.0.0-20230427085034-04cfb63088f8
 	github.com/eatmoreapple/openwechat v1.4.3
@@ -22,6 +23,7 @@ require (
 	github.com/AlekSi/pointer v1.2.0 // indirect
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/xpath v1.2.3 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
@@ -30,6 +32,7 @@ require (
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
@@ -45,11 +48,11 @@ require (
 	github.com/tidwall/gjson v1.14.3 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10 // indirect
+	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/term v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/term v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
