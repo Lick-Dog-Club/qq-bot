f2cd1ef84f501bd1a70e41db0d57755b07e6e5cb
duc <1025434218@qq.com>
2023-07-04 16:09:22 +0800

    wip

--- cronjob/maotai/maotai.go
+++ cronjob/maotai/maotai.go
@@ -1,14 +1,21 @@
 package maotai
 
 import (
+	"fmt"
 	"qq/bot"
 	"qq/config"
 	"qq/cronjob"
+	"qq/features/imaotai"
 )
 
 func init() {
 	cronjob.Manager().NewCommand("maotai", func(robot bot.CronBot) error {
-		robot.SendGroup(config.GroupID(), "开始申购茅台了~")
+		var res string
+		for _, info := range config.MaoTaiInfoMap() {
+			res += fmt.Sprintf("%s:\n%s\n", info.Phone, imaotai.Run(info.Phone))
+		}
+		robot.SendGroup(config.GroupID(), "茅台申购结束")
+		robot.SendGroup(config.GroupID(), res)
 		return nil
 	}).DailyAt("9:15")
 }
