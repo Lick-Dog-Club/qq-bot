5d2a86239f2f29a439e480e0a09c465a8b2d401b
duc <1025434218@qq.com>
2023-07-05 10:42:24 +0800

    wip

--- cronjob/maotai/maotai.go
+++ cronjob/maotai/maotai.go
@@ -12,6 +12,10 @@ func init() {
 	cronjob.Manager().NewCommand("maotai", func(robot bot.CronBot) error {
 		var res string
 		for _, info := range config.MaoTaiInfoMap() {
+			if info.Expired() {
+				res += fmt.Sprintf("%s: token已过期，需要重新登陆", fuzzyPhone(info.Phone))
+				continue
+			}
 			res += fmt.Sprintf("%s:\n%s\n", fuzzyPhone(info.Phone), imaotai.Run(info.Phone))
 		}
 		robot.SendGroup(config.GroupID(), "茅台申购结束")
