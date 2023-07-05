781c9e6bcd63da2fbba20017753866b1e8a15781
duc <1025434218@qq.com>
2023-07-05 10:40:24 +0800

    wip

--- cronjob/maotai/maotai.go
+++ cronjob/maotai/maotai.go
@@ -12,10 +12,17 @@ func init() {
 	cronjob.Manager().NewCommand("maotai", func(robot bot.CronBot) error {
 		var res string
 		for _, info := range config.MaoTaiInfoMap() {
-			res += fmt.Sprintf("%s:\n%s\n", info.Phone, imaotai.Run(info.Phone))
+			res += fmt.Sprintf("%s:\n%s\n", fuzzyPhone(info.Phone), imaotai.Run(info.Phone))
 		}
 		robot.SendGroup(config.GroupID(), "茅台申购结束")
 		robot.SendGroup(config.GroupID(), res)
 		return nil
 	}).DailyAt("9:15")
+}
+
+func fuzzyPhone(phone string) string {
+	if len(phone) == 11 {
+		phone = phone[0:3] + "****" + phone[7:11]
+	}
+	return phone
 }
