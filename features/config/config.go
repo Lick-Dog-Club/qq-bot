2d5b17c2aa2d73176337b782c7598b0d9812ce55
duc <1025434218@qq.com>
2023-07-10 14:00:07 +0800

    wip

--- features/config/config.go
+++ features/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"bufio"
+	"fmt"
 	"qq/bot"
 	"qq/config"
 	"qq/features"
@@ -15,15 +17,20 @@ func init() {
 			return nil
 		}
 		var conf = map[string]string{}
-		split := strings.Split(content, ",")
-		for _, s := range split {
-			n := strings.SplitN(s, "=", 2)
+
+		scanner := bufio.NewScanner(strings.NewReader(content))
+		for scanner.Scan() {
+			n := strings.SplitN(scanner.Text(), "=", 2)
 			if len(n) == 2 {
 				conf[n[0]] = n[1]
 			}
 		}
+		if scanner.Err() != nil {
+			bot.Send(fmt.Sprintf("%e", scanner.Err()))
+		}
+
 		config.Set(conf)
-		bot.Send("已设置: " + content)
+		bot.Send("已设置: \n" + config.Configs().String())
 		return nil
 	}, features.WithSysCmd(), features.WithHidden(), features.WithGroup("config"))
 	features.AddKeyword("cg", "显示环境变量", func(bot bot.Bot, content string) error {
