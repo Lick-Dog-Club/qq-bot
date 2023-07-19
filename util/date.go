7b2d6bf5399c45ded07e65474073105c5cb99327
duc <1025434218@qq.com>
2023-07-19 10:15:02 +0800

    wip

--- util/date.go
+++ util/date.go
@@ -6,3 +6,11 @@ func Today() time.Time {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 }
+
+func Yesterday() time.Time {
+	return Today().Add(-24 * time.Hour)
+}
+
+func Tomorrow() time.Time {
+	return Today().Add(24 * time.Hour)
+}
