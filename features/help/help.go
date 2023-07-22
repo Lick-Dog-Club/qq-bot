896131d558720a13b4d43545329914a84266da76
duc <1025434218@qq.com>
2023-07-22 22:45:16 +0800

    wip

--- features/help/help.go
+++ features/help/help.go
@@ -11,6 +11,7 @@ import (
 )
 
 var once sync.Once
+var hideOnce sync.Once
 
 func init() {
 	features.AddKeyword("help", "帮助信息", func(sender bot.Bot, content string) error {
@@ -25,15 +26,24 @@ func init() {
 
 func showHelp(bot bot.Bot, hidden bool) {
 	fpath := filepath.Join("/data", "images", "help.png")
+	hiddenFpath := filepath.Join("/data", "images", "hidden-help.png")
+	hideOnce.Do(func() {
+		text2png.Draw(features.BeautifulOutputLines(hidden, true), hiddenFpath)
+	})
 	once.Do(func() {
 		text2png.Draw(features.BeautifulOutputLines(hidden, true), fpath)
 	})
 
+	var p string = fpath
+	if hidden {
+		p = hiddenFpath
+	}
+
 	if bot.Message().WeSendImg != nil {
-		open, _ := os.Open(fpath)
+		open, _ := os.Open(p)
 		defer open.Close()
 		bot.Message().WeSendImg(open)
 	} else {
-		bot.Send(fmt.Sprintf("[CQ:image,file=file://%s]", fpath))
+		bot.Send(fmt.Sprintf("[CQ:image,file=file://%s]", p))
 	}
 }
