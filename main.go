0bc33918f3f8772bea9f815ca098164f9fe67650
duc <1025434218@qq.com>
2023-08-26 03:08:53 +0800

    wip

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	//_ "qq/cronjob/ddys"
 
 	_ "qq/cronjob/autoupdate"
-	//_ "qq/cronjob/btc"
+	_ "qq/cronjob/btc"
 	_ "qq/cronjob/comic"
 	_ "qq/cronjob/dx"
 	_ "qq/cronjob/lottery"
@@ -40,7 +40,7 @@ import (
 	_ "qq/features/help"
 	_ "qq/features/imaotai"
 
-	//_ "qq/features/jin10"
+	_ "qq/features/jin10"
 	_ "qq/features/kfc"
 	_ "qq/features/lifetip"
 	_ "qq/features/picture"
