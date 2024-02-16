9f1cae57ab8e9d7b5d9f9423903f8077cd588d2a
duc <1025434218@qq.com>
2024-02-16 16:34:19 +0800

    wip

--- features/ai/api/api.go
+++ features/ai/api/api.go
@@ -151,7 +151,7 @@ func (gpt *chatGPTClient) send(msg string) string {
   你需要返回: [CQ:image,file=file:///tmp/a.png]
 
 3. 如果用户查询高铁火车票信息, 按照以下步骤处理
-    - 需要 GetStationCodeByName 查询对应的车站 code
+    - 需要 GetStationCodeByName 查询对应的车站 code, 如果返回code 为空，那么需要从 StationNamesJson 中找出目的地相关的车站对应 code，例如目的地是 “杭州”，GetStationCodeByName 找不到对应 code，但是 StationNamesJson 有 “杭州东”，那么使用 “杭州东”
     - 再调用 Search12306 查询班次信息
     - 没告诉你时间就那么默认是今天
     - 一等座、二等座和无座已售罄的车次无需告诉用户，重点关注二等座，二等座优先级最高, 如果二等座都卖完了，可以告诉用户其他可选的班次
