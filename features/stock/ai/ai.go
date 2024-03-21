24ccf9ea6bc69171aaaf7203388301b601446c95
duc <1025434218@qq.com>
2024-03-22 01:21:15 +0800

    wip

--- features/stock/ai/ai.go
+++ features/stock/ai/ai.go
@@ -74,9 +74,10 @@ func (h *History) Add(message openai.ChatCompletionMessage) {
 	h.list = append(h.list, message)
 	tokens := LastConversationsByLimitTokens(h.list, 4096)
 	for len(tokens) > 0 {
-		if tokens[0].Role == openai.ChatMessageRoleTool {
-			tokens = tokens[1:]
+		if tokens[0].Role != openai.ChatMessageRoleTool {
+			break
 		}
+		tokens = tokens[1:]
 	}
 	h.list = tokens
 }
